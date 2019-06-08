335ef84622ce21c7023523e73e682345c6a113a2
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2019-06-08 12:18:07 -0300

    full commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'bitSeguros';
-}
+  constructor(private http: HttpClient) { }
+
+  result = {erro: true};
+  validInput = false;
+  apiURL = '';
+
+  cepResults(inputValue) {
+    this.apiURL = 'https://viacep.com.br/ws/' + inputValue + '/json/'
+    this.validInput = true;
+    this.http.get(this.apiURL).subscribe(
+      result => {
+        this.result = result;
+        // console.log(this.result);
+      }
+    );
+
+}}
